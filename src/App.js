a1a0bdaf42fa61df2b69fb5355264dbbeb49a54c
Rakesh Pandit <140710970+Rakesh-Pandit-Developer@users.noreply.github.com>
2025-02-24 08:15:07 +0545

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import AOS from 'aos';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import './App.css';
+
+function App() {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+  
+  return (
+    <div className="App">
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
